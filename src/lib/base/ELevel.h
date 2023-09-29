61c106ed070b68bc8c2065efe4552d91320aff70
Serhii Hadzhilov <gadzhilov.sergey@gmail.com>
2023-09-29 16:49:06 +0200

    Update comments headers

--- src/lib/base/ELevel.h
+++ src/lib/base/ELevel.h
@@ -1,5 +1,5 @@
 /*
- * InputLeap -- mouse and keyboard sharing utility
+ * SKVM -- mouse and keyboard sharing utility
  * Copyright (C) 2012-2016 Symless Ltd.
  * Copyright (C) 2011 Chris Schoeneman
  *
