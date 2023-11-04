2908dc83da1ca0f892530d702f78a2f5bde62e83
Serhii Hadzhilov <gadzhilov.sergey@gmail.com>
2023-11-04 15:22:53 +0200

    Fix for windows pack

--- dist/install.cmake
+++ dist/install.cmake
@@ -6,14 +6,7 @@ if(APPLE)
 endif()
 
 if(WIN32)
-   install(TARGETS skvm skvmc skvms
-        BUNDLE DESTINATION . COMPONENT "SKVM"
-        LIBRARY DESTINATION ${CMAKE_INSTALL_LIBDIR} COMPONENT "SKVM"
-        RUNTIME DESTINATION ${CMAKE_INSTALL_LIBDIR} COMPONENT "SKVM"
-    )
-   install(FILES ${OPENSSL_COMPONENTS} DESTINATION bin COMPONENT "SKVM")
-   install(TARGETS skvmd COMPONENT "SKVM")
-   include(dist/win/win.cmake)
+   include(dist/win/install.cmake)
 endif()
 
 if(UNIX AND NOT APPLE)
@@ -22,5 +15,5 @@ if(UNIX AND NOT APPLE)
         LIBRARY DESTINATION ${CMAKE_INSTALL_LIBDIR} COMPONENT "SKVM"
         RUNTIME DESTINATION ${CMAKE_INSTALL_LIBDIR} COMPONENT "SKVM"
     )
-    include(dist/linux/linux.cmake)
+    include(dist/linux/install.cmake)
 endif()
