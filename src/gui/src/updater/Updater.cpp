534ce46cacf344ceaf44944edcea10231ba5ab77
Serhii <gadzhilov.sergey@gmail.com>
2024-01-24 20:04:04 +0200

    Implement updater class to check for new updates

---
+++ src/gui/src/updater/Updater.cpp
@@ -0,0 +1,76 @@
+/*
+ * SKVM -- mouse and keyboard sharing utility
+ * Copyright (C) 2024 Hadzhilov Serhii
+ *
+ * This package is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * found in the file LICENSE that should have accompanied this file.
+ *
+ * This package is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include <iostream>
+#include "Updater.h"
+#include <QNetworkReply>
+#include <QJsonDocument>
+#include <QJsonObject>
+#include <QNetworkRequest>
+
+namespace skvm {
+namespace updater {
+
+Updater::Updater(const std::string& version) :
+    m_current(version)
+{
+}
+
+void Updater::CheckForUpdate()
+{
+    connect(&m_network, &QNetworkAccessManager::finished, this, [this](QNetworkReply* reply) {
+        if (!reply)
+        {
+            std::cerr<<"bad response from the update server"<<std::endl;
+            return;
+        }
+
+        const auto payload = reply->readAll();
+        reply->deleteLater();
+        if (payload.isEmpty())
+        {
+            std::cerr<<"empty response from the update server"<<std::endl;
+            return;
+        }
+
+        const QJsonDocument response = QJsonDocument::fromJson(payload);
+        if (response.isNull() || response.isEmpty())
+        {
+            std::cerr<<"fail to parse response from the update server"<<std::endl;
+            return;
+        }
+
+        const auto tag = response.object().value("tag_name");
+        if (tag.isUndefined())
+        {
+            std::cerr<<"field tag_name doesn't exist in the response from update server"<<std::endl;
+            return;
+        }
+
+        Version latest(tag.toString().toStdString());
+        if (latest > m_current)
+        {
+            std::cout<<"new version "<<latest<<" is available"<<std::endl;
+            emit NewVersionAvailable(latest);
+        }
+    });
+
+    QNetworkRequest request(QUrl("https://api.github.com/repos/SergeyGadzhilov/SKVM/releases/latest"));
+    m_network.get(request);
+}
+
+} //namespace updates
+} //namespace skvm
---
+++ src/gui/src/updater/Version.cpp
@@ -0,0 +1,77 @@
+/*
+ * SKVM -- mouse and keyboard sharing utility
+ * Copyright (C) 2024 Hadzhilov Serhii
+ *
+ * This package is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * found in the file LICENSE that should have accompanied this file.
+ *
+ * This package is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include <iostream>
+#include "Version.h"
+#include <base/String.h>
+
+namespace skvm {
+namespace updater {
+
+Version::Version()
+{
+}
+
+Version::Version(const std::string& version)
+{
+    parse(version);
+}
+
+bool Version::operator >(const Version& version) const
+{
+    if (m_major > version.m_major) return true;
+    if (m_major < version.m_major) return false;
+    if (m_minor > version.m_minor) return true;
+    if (m_minor < version.m_minor) return false;
+    if (m_patch > version.m_patch) return true;
+
+    return false;
+}
+
+std::ostream& operator<<(std::ostream &stream, const Version& version)
+{
+    stream<<"v"<<version.m_major<<"."<<version.m_minor<<"."<<version.m_patch;
+    return stream;
+}
+
+void Version::parse(const std::string& version)
+{
+    try {
+        if (version.empty())
+        {
+            std::cerr<<"fail to parse version because it's empty"<<std::endl;
+            return;
+        }
+
+        auto parts = string::splitString(version, '.');
+        if (parts.size() < 3)
+        {
+            std::cerr<<"fail to parse version: "<<version<<std::endl;
+            return;
+        }
+
+        m_major = std::stoi(parts[0]);
+        m_minor = std::stoi(parts[1]);
+        m_patch = std::stoi(parts[2]);
+    }
+    catch (std::exception& ex)
+    {
+        std::cerr<<"fail to parse version: "<<version<<" because "<<ex.what()<<std::endl;
+    }
+}
+
+} //namespace updates
+} //namespace skvm
